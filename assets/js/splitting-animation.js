f5e8f6b1245af96c34b661533900c472d92bac87
Arathi-k-v <arathi@signroots.com>
2026-06-17 11:06:41 +0530

    Files Uploaded

---
+++ assets/js/splitting-animation.js
@@ -0,0 +1,13 @@
+(function ($) {
+	("use strict");
+
+
+	// WOW Animatin area start here ***
+	Splitting();
+	wow = new WOW({
+		animateClass: "animated",
+		offset: 100,
+	});
+	wow.init();
+	// WOW Animatin area start here ***
+})(jQuery);
